b96a3de3893f88620f66485df6526ab0e0d0df34
JuanCalderon <Juanma1704@gmail.com>
2024-08-04 15:37:15 -0500

    ACTUALIZACION

--- server.js
+++ server.js
@@ -9,33 +9,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 //Conectar a MongoDB
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Juan:<password>@jmch.qtlzygo.mongodb.net/?retryWrites=true&w=majority&appName=JMCH";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
+mongoose.connect('mongodb+srv://Juan:16172425@jmch.qtlzygo.mongodb.net/?retryWrites=true&w=majority&appName=JMCH', {
+    useNewUrlParser: true, useUnifiedTopology: true })
+    .then (() => console.log('Conectado a MongoDB'))
+    .catch(err => console.log(err));
 //Definir el esquema y modelo de usuario
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true}
